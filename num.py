2cee8d5edab48457696405491b5ffe27d9444921
周恩吉 <zhouenji@zhouenjideAir.realtek>
2020-06-22 21:34:04 +0800

    final version

--- num.py
+++ num.py
@@ -1,7 +1,13 @@
 import random
+start = input('请决定数字范围开始值：')
+end = input('请决定数字范围结束值：')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
+count = 0
 while True:
+	count += 1
 	num = input('猜数字: ')
 	num = int(num)
 	if num == r:
@@ -11,3 +17,4 @@ while True:
 		print('比答案大')
 	elif num < r:
 		print('比答案小')
+	print('这是你猜的第', count, '次')
